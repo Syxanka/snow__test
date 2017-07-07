fb2c683256f8fe6abe80ec951c1c04dd7d2a9bd9
Kristina Manchenko <cristal89@bk.ru>
2017-07-07 16:05:24 +0300

    добавлены картинки,стили, animation

--- js__skript.js
+++ js__skript.js
@@ -33,19 +33,19 @@ $(document).ready(function(){
 
 //анимация при наведение на блок animated
 
-function animate(elem){
-    var effect = elem.data("effect");
-    if(!effect || elem.hasClass(effect)) return false;
-    elem.addClass(effect);
-    setTimeout( function(){
-        elem.removeClass(effect);
-    }, 1000);
-}
- 
-$(".animated").mouseenter(function() {
-    animate($(this));
-});
+//function animate(elem){
+//    var effect = elem.data("effect");
+//    if(!effect || elem.hasClass(effect)) return false;
+//    elem.addClass(effect);
+//    setTimeout( function(){
+//        elem.removeClass(effect);
+//    }, 1000);
+//}
+// 
+//$(".animated").mouseenter(function() {
+//    animate($(this));
+//    });
 
-		});
+});
 
 
