ffc97fc9ecd799201c25d6c78ca4f6e5f22cd8ea
Kristina Manchenko <cristal89@bk.ru>
2017-07-15 17:07:45 +0300

    обновление меню, адаптива

--- js__skript.js
+++ js__skript.js
@@ -38,5 +38,12 @@ $(document).ready(function(){
       return false;
     });
     
+    $("ul li a").click(function () { 
+      var elementClick = $(this).attr("href");
+      var destination = $(elementClick).offset().top;
+      $('html,body').animate( { scrollTop: destination }, 900 );
+      return false;
+    });
+    
       });
 
