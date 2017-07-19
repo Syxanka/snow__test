6fa3c6024506d0a9f1d96a654c2be0c2cb801c93
Kristina Manchenko <cristal89@bk.ru>
2017-07-19 08:31:53 +0300

    изменение разметки portfolio и index.html+css

--- js__skript.js
+++ js__skript.js
@@ -59,5 +59,13 @@ $(document).ready(function(){
       return false;
     });
     
+//плавный скролл portfolio.html , блок more   
+ $(".more a").click(function () { 
+      var elementClick = $(this).attr("href");
+      var destination = $(elementClick).offset().top;
+      $('html,body').animate( { scrollTop: destination }, 900 );
+      return false;
+    });    
+    
       });
 
